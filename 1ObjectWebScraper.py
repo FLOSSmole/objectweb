8ac55d524cbabf16099752b13f3fa956fe2251b6
cmfrankel <cfrankel@elon.edu>
2017-06-12 16:05:17 -0400

    Create 1ObjectWebScraper.py

--- 1ObjectWebScraper.py
+++ 1ObjectWebScraper.py
@@ -31,65 +31,76 @@
 # get master list of ObjectWeb projects and add basic facts to database
 ################################################################
 
-import urllib.request
-import re
+from bs4 import BeautifulSoup
 import sys
 import pymysql
 import datetime
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
 
 datasource_id = sys.argv[1]
 password      = sys.argv[2]
 
-
 # establish database connection: SYR
-try:
-    db = pymysql.connect(host='flossdata.syr.edu',
-                     user='cfrankel',
-                     passwd='Marco1997',
-                     db='test',
+db = pymysql.connect(host='flossdata.syr.edu',
+                     user='',
+                     passwd='',
+                     db='',
                      use_unicode=True,
                      charset="utf8mb4")
-    cursor = db.cursor()
-except pymysql.Error as err:
-    print(err)
+cursor = db.cursor()
 
-# Get list of all projects & urls from the database
-selectQuery = 'SELECT proj_unixname, url FROM ow_projects ' \
-              'WHERE datasource_id=%s ORDER BY 1'
-updateProjectQuery = 'UPDATE ow_projects ' \
-                     'SET ' \
-                     'date_registered = %s,' \
-                     'date_collected = %s' \
-                     'WHERE proj_unixname = %s' \
-                     'AND datasource_id = %s;'
+insertQuery = 'INSERT INTO ow_projects  \
+                         (proj_unixname,  \
+                         url, \
+                         proj_long_name, \
+                         datasource_id, \
+                         date_collected) \
+                         VALUES (%s,%s,%s,%s,%s)'
+
+# set up headers
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
+# Get page that lists all projects
 try:
-    cursor.execute(selectQuery, (datasource_id))
-    listOfProjects = cursor.fetchall()
+    projectListURL = 'https://forge.ow2.org/softwaremap/full_list.php'
 
-    for project in listOfProjects:
-        currentProject = project[0]
-        projectOWUrl = project[1]
-        print('working on', currentProject)
-        try:
-            projectPage = urllib.request.urlopen(projectOWUrl)
-            myPage = projectPage.read().decode('utf-8')
-            results = re.findall('Registered:&nbsp;(.*?)\s(\d\d:\d\d)', myPage)
+    req = urllib2.Request(projectListURL, headers=hdr)
+    projectListPage = urllib2.urlopen(req).read()  # this is a HTML
 
-            if results:
-                regDate = results[0][0] + ' ' + results[0][1]
-                print('Registration date:', regDate)
+    soup = BeautifulSoup(projectListPage, "html.parser")
+    td = soup.find_all('select', attrs={'name': 'navigation'})
 
-            try:
-                cursor.execute(updateProjectQuery,
-                                (regDate,
-                                 datetime.datetime.now(),
-                                 currentProject,
-                                 datasource_id))
-                db.commit()
-            except pymysql.Error as err:
-                print(err)
-                db.rollback()
-        except urllib.error.URLError as e:
-            print(e.reason)
-except pymysql.Error as err:
-    print(err)
+    urlStem = 'http://forge.objectweb.org/projects/'
+
+    for line in td:
+        projectOptions = line.find_all('option')
+
+        for option in projectOptions:
+            projectURL = option.get('value')
+            projectLongName = option.text
+
+            if projectURL and urlStem in projectURL:
+                projectShortName = projectURL[len(urlStem):
+
+                print('working on', projectShortName)
+                try:
+                    cursor.execute(insertProjectQuery, 
+                         (projectShortName, 
+                          projectURL,
+                          projectLongName,
+                          datasource_id,
+                          datetime.datetime.now()))
+                    db.commit()
+                except pymysql.Error as err:
+                    print(err)
+                    db.rollback() 
+except urllib.error.URLError as e:
+    print(e.reason)
