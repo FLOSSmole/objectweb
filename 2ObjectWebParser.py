0ea638a4807e4ea8e0876076d6a61c3d10f05656
Megan Squire <megansquire@gmail.com>
2017-06-22 12:42:20 -0400

    Update 2ObjectWebParser.py

--- 2ObjectWebParser.py
+++ 2ObjectWebParser.py
@@ -33,15 +33,12 @@
 import sys
 import pymysql
 import re
-import datetime
-from bs4 import BeautifulSoup
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
 
 datasource_id = sys.argv[1]
-password      = sys.argv[2]
+dbpw = sys.argv[2]
+dbhost = 'flossdata.syr.edu'
+dbuser = 'megan'
+dbschema = 'objectweb'
 
 homePageUrl = None
 regDate = None
@@ -50,29 +47,31 @@ devCount = None
 
 # establish database connection: SYR
 try:
-    db = pymysql.connect(host='flossdata.syr.edu',
-                         user='',
-                         passwd='',
-                         db='',
-                         use_unicode=True,
-                         charset="utf8mb4")
-    cursor = db.cursor()
+    dbconn = pymysql.connect(host=dbhost,
+                             user=dbuser,
+                             passwd=dbpw,
+                             db=dbschema,
+                             use_unicode=True,
+                             charset="utf8mb4")
+    cursor = dbconn.cursor()
 except pymysql.Error as err:
     print(err)
 
 # Get list of all projects & urls from the database
-selectQuery = 'SELECT proj_unixname, url, datasource_id FROM ow_projects \
-              WHERE datasource_id=%s ORDER BY 1'
+selectQuery = 'SELECT proj_unixname, url \
+               FROM ow_projects \
+               WHERE datasource_id=%s \
+               ORDER BY 1'
 
 updateProjectQuery = 'UPDATE ow_projects \
-                     SET  \
-                     real_url = %s, \
-                     date_registered = %s, \
-                     proj_id_num =%s, \
-                     dev_count = %s, \
-                     date_collected = %s \
-                     WHERE proj_unixname = %s \
-                     AND datasource_id = %s;'
+                      SET \
+                      real_url = %s, \
+                      date_registered = %s, \
+                      proj_id_num =%s, \
+                      dev_count = %s, \
+                      date_collected = now() \
+                      WHERE proj_unixname = %s \
+                      AND datasource_id = %s'
 
 # set up headers
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -89,21 +88,27 @@ try:
     for project in listOfProjects:
         currentProject = project[0]
         projectOWUrl = project[1]
-        datasource_id = project[2]
         print('\nworking on', currentProject)
 
         try:
+            # TO-DO: replace this part
+            # use the index html that we collected in 1ObjectWebScraper.py
+            # then, replace the 'soup' in the lines below
+            '''
             projectPage = urllib2.Request(projectOWUrl, headers=hdr)
             myPage = urllib2.urlopen(projectPage).read()
             soup = BeautifulSoup(myPage, "html.parser")
+            '''
 
+            # Parse out homepage
             regex1 = '<a href="(.*?)"><img alt="Home Page"'
             match1 = re.findall(regex1, str(soup))[0]
 
             if match1:
                 homePageUrl = match1
-                print(homePageUrl)
+                print("--homepage:", homePageUrl)
 
+            # Parse out registration date
             regex2 = 'Registered:(.*?)\d(.*?)\s*<'
             match2 = re.findall(regex2, str(soup))[0]
 
@@ -111,21 +116,23 @@ try:
                 for m in match2:
                     if len(m) > 5:
                         regDate = '2' + m
-                        print('Registration date:', regDate)
+                        print('--registration date:', regDate)
 
+            # Parse out group id
             regex3 = '<input name=\"group_id\" type=\"hidden\" value=\"(.*?)\"/>'
             match3 = re.findall(regex3, str(soup))[0]
 
             if match3:
                 groupId = match3
-                print("group id: ", groupId)
+                print("--group id: ", groupId)
 
+            # Parse out Developer count
             regex4 = 'class=\"develtitle\">Developers:</span><br/>\s*(.*?)\s*<a'
             match4 = re.findall(regex4, str(soup))[0]
 
             if match4:
                 devCount = match4
-                print("dev count: ", devCount)
+                print("--dev count: ", devCount)
 
             try:
                 cursor.execute(updateProjectQuery,
@@ -133,14 +140,13 @@ try:
                                 regDate,
                                 groupId,
                                 devCount,
-                                datetime.datetime.now(),
                                 currentProject,
                                 datasource_id))
-                db.commit()
+                dbconn.commit()
             except pymysql.Error as err:
                 print(err)
-                db.rollback()
+                dbconn.rollback()
         except pymysql.Error as err:
-            print(e.reason)
+            print(err.reason)
 except pymysql.Error as err:
     print(err)
